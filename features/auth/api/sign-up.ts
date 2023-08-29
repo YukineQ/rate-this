868b0bff3a035ad2f4981f9b7d5b677dcb2b0c60
YukineQ <mayberainq@gmail.com>
2023-08-29 19:49:27 +0300

    ......../

--- features/auth/api/sign-up.ts
+++ features/auth/api/sign-up.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from "axios";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
 
-import { User } from "@prisma/client";
+// import { User } from "@prisma/client";
 
 export type SignUpCredendialsDTO = {
     username: string;
@@ -12,7 +12,7 @@ export type SignUpCredendialsDTO = {
 
 export const signUpWithCredentials = (
     data: SignUpCredendialsDTO
-): Promise<User> => {
+) => {
     return axios.post('/api/register', data)
 }
 
